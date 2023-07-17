20a627528e53678e16e8aae70cb7280c34dbef7e
khouloud HADDAD AMAMOU <khouloud.amamou@azurreo.com>
2023-07-17 07:19:34 +0100

    Project Setup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>
-        @yield('title', 'SaaS') | {{ config('app.name')}
+        @yield('title', 'SaaS') | {{ config('app.name') }}
         </title>
 
         <!-- Fonts -->
